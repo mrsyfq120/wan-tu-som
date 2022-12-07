aae98f2e5a8176754e77dcc55595ddac5e2a3327
Alif Syafiq <alfiqnasri@gmail.com>
2022-12-07 11:55:30 +0800

    Add a randomize function

---
+++ javascript.js
@@ -0,0 +1,12 @@
+// declare function with arguments
+function getComputerChoice(types) {
+// set the initial value to change
+ for (let i = 0; i < 1; i++) {
+// declare variables that caused things to randomize
+  const random = types[Math.floor(Math.random() * types.length)];
+    return random;
+ }
+}
+
+// set the name of the types
+let names = ['Rock', 'Paper', 'Scissors'];
