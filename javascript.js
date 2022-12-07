9aa046f5c622627500e90b6035368cb14b3e4204
Alif Syafiq <alfiqnasri@gmail.com>
2022-12-07 12:01:18 +0800

    Make the comments easier to understand

--- javascript.js
+++ javascript.js
@@ -1,6 +1,6 @@
 // declare function with arguments
 function getComputerChoice(types) {
-// set the initial value to change
+// set the initial number so that it could add the probability of randomize
  for (let i = 0; i < 1; i++) {
 // declare variables that caused things to randomize
   const random = types[Math.floor(Math.random() * types.length)];
